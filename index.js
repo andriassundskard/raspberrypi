801039b13c6e0dc806088142930a5b13c30fb129
Andrias Sundskarð <andrias@sundskard.dk>
2017-11-16 12:54:38 +0100

    Working example

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿const {app, BrowserWindow} = require("electron");
+
+let mainWindow = null;
+
+app.on("ready", function() {
+	mainWindow = new BrowserWindow({
+		width: 400,
+		height: 300,
+		show: false
+	});
+	mainWindow.loadURL("file://${ __dirname }/index.html");
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
+	});
+	mainWindow.on("closed", function() {
+		mainWindow = null;
+	});
+});
